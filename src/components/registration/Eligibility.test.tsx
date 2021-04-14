5572fd5cb35a8bda221d95904ab646b8ca1b39ac
RicardoFrabasile <rfrabasile@gmail.com>
2021-04-14 12:03:17 -0300

    Added event capturing, title and param changes, removed country from storage

--- src/components/registration/Eligibility.test.tsx
+++ src/components/registration/Eligibility.test.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Eligibility from './Eligibility'
 import TestRenderer from 'react-test-renderer'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 test('renders Eligibility', () => {
   const testRenderer = TestRenderer.create(
-    <Eligibility setEligibilityFn={() => {}} />,
+    <Router>
+      <Eligibility setEligibilityFn={() => {}} />
+    </Router>,
   )
   const testInstance = testRenderer.root
   expect(testInstance.findByType(Eligibility))
