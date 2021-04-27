a26f5d7063f05ed38a2280c22a82753e25c39e17
William Martinez <william.martinez@genui.com>
2021-04-27 17:35:08 -0600

    WIP - desktop download app page

--- src/components/dashboard/DownloadApp.tsx
+++ src/components/dashboard/DownloadApp.tsx
@@ -5,17 +5,20 @@ function DownloadApp() {
   return (
     <div className="download-app">
       <div className="download--not-installed">
-        <LogoNoText />
+        <LogoNoText className="logo" />
         <h1 className="download__title">Download the MindKind app</h1>
         <p>
           Some placeholder text about the app and what you may do in the app.
         </p>
-        <button className="download__button">Download</button>
+        <button className="download__button">Download the app</button>
       </div>
       <div className="download--already-installed">
         <h1 className="download__title">If you already have the app</h1>
         <p>Click here to log-in to the app and begin the study!</p>
         <button className="download__button">Sign In to the App</button>
+      </div>
+      <div className="phone">
+        <div className="phone__bg"></div>
       </div>
     </div>
   )
