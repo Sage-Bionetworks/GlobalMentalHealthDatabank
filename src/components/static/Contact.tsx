8a55627508f5a00a5291fbd0d701ab84d6acf945
William Martinez <william.martinez@genui.com>
2021-05-11 11:17:04 -0600

    moved texts to translation file

--- src/components/static/Contact.tsx
+++ src/components/static/Contact.tsx
@@ -1,68 +1,76 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { useTranslation } from 'react-i18next'
 
 function Contact() {
+  const { t } = useTranslation()
   return (
     <div className="contact">
       <div className="contact__in-crisis">
         <div className="plus-signs" />
-        <Typography variant="h2">If you are in a crisis</Typography>
-        <Typography variant="body1">
-          If you are in crisis, or feel that you are going to hurt yourself or
-          others, please contact your healthcare provider. Additional resources
-          provided below.
-        </Typography>
+        <Typography variant="h2">{t('contact.title1')}</Typography>
+        <Typography variant="body1">{t('contact.subtitle1')}</Typography>
       </div>
       <div className="contact__get-help">
-        <Typography variant="h4">Get help in your region</Typography>
-        <button>Find Help</button>
+        <Typography variant="h4">{t('contact.help.get')}</Typography>
+        <button>{t('contact.help.find')}</button>
       </div>
       <div className="contact__local-resource">
-        <Typography variant="h2">Local Resources in India</Typography>
-        <Typography variant="body1">
-          iCallHelpline provides free counseling.
+        <Typography variant="h2">
+          {t('contact.localResources.india.title')}
         </Typography>
         <Typography variant="body1">
-          The iCallHelpline is available Monday-Saturday, 10am-8pm. Call
-          022-25521111
+          {t('contact.localResources.india.text1')}
         </Typography>
         <Typography variant="body1">
-          You can also email iCallHelpline at icall@tiss.edu for a response from
-          a counselor within 24 hours.
+          {t('contact.localResources.india.text2')}
         </Typography>
-        <button className="small">Visit Website</button>
+        <Typography variant="body1">
+          {t('contact.localResources.india.text3')}
+        </Typography>
+        <button className="small">
+          {t('contact.localResources.india.link.text')}
+        </button>
       </div>
       <div className="contact__local-resource">
-        <Typography variant="h2">Local Resources in South Africa</Typography>
-        <Typography variant="body1">
-          SADAG is available for emotional support.
+        <Typography variant="h2">
+          {t('contact.localResources.southAfrica.title')}
         </Typography>
         <Typography variant="body1">
-          The SADAG hotline is available 24/7 to provide assistance.{' '}
+          {t('contact.localResources.southAfrica.text1')}
         </Typography>
-        <Typography variant="body1">Call 0800456789 </Typography>
         <Typography variant="body1">
-          You can also email SADAG at Zane@sadag.org for information about
-          counseling.
+          {t('contact.localResources.southAfrica.text2')}
         </Typography>
-        <button className="small">Visit Website</button>
+        <Typography variant="body1">
+          {t('contact.localResources.southAfrica.text3')}
+        </Typography>
+        <Typography variant="body1">
+          {t('contact.localResources.southAfrica.text4')}
+        </Typography>
+        <button className="small">
+          {t('contact.localResources.southAfrica.link.text')}
+        </button>
       </div>
       <div className="contact__local-resource">
-        <Typography variant="h2">Local Resources in the UK</Typography>
-        <Typography variant="body1">
-          Samaritans UK is available for emotional support.
+        <Typography variant="h2">
+          {t('contact.localResources.unitedKingdom.title')}
         </Typography>
         <Typography variant="body1">
-          The Samaritans UK hotline is available 24/7.{' '}
+          {t('contact.localResources.unitedKingdom.text1')}
         </Typography>
         <Typography variant="body1">
-          For English, call 116 123 For Welsh, call l 0808 164 0123
+          {t('contact.localResources.unitedKingdom.text2')}
         </Typography>
         <Typography variant="body1">
-          You can also email Samaritans UK at jo@samaritans.org for a response
-          from a counselor within 24 hours.
+          {t('contact.localResources.unitedKingdom.text3')}
         </Typography>
-        <button className="small">Visit Website</button>
+        <Typography variant="body1">
+          {t('contact.localResources.unitedKingdom.text4')}
+        </Typography>
+        <button className="small">
+          {t('contact.localResources.unitedKingdom.link.text')}
+        </button>
       </div>
     </div>
   )
