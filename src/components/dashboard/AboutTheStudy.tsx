a7bf72b7d7f75f65b0360357a8b74f8da0b8799d
Brad Henslee <bradh@genui.com>
2021-07-09 11:13:11 -0700

    open link in new tab

--- src/components/dashboard/AboutTheStudy.tsx
+++ src/components/dashboard/AboutTheStudy.tsx
@@ -598,7 +598,12 @@ function AboutTheStudy({
             </Typography>
             <Typography variant="body2">
               {t('form.firstCommonConsent.ifCitizenEU')}{' '}
-              <a className="underlined-link" href={ROUTES.DATA_REGULATION}>
+              <a
+                className="underlined-link"
+                href={ROUTES.DATA_REGULATION}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {t('form.firstCommonConsent.ifCitizenEULink')}
               </a>
             </Typography>
