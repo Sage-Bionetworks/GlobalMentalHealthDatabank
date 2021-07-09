d1b36d72802debf83fa70df414f04bf489d90e58
Brad Henslee <bradh@genui.com>
2021-07-08 18:43:24 -0700

    WIP EOD (actual)

--- src/components/static/Terms.tsx
+++ src/components/static/Terms.tsx
@@ -8,191 +8,256 @@ export const Terms: React.FunctionComponent = () => {
   return (
     <ResponsiveStepWrapper variant="card">
       <div className="legislation-wrapper">
-        <div className="btm-40">
+        <div className="btm-20">
           <Typography variant="h3">{t('terms.title')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text1')}</Typography>
+          <Typography variant="body2">
+            {t('terms.effectiveDateText')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text2')}</Typography>
+          <Typography variant="body2">{t('terms.mindkindStudy1')}</Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">{t('terms.mindkindStudy2')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text3')}</Typography>
+          <Typography variant="h4">{t('terms.whoCanUse')}</Typography>
+        </div>
+        <div className="btm-20 bullets">
+          <Typography variant="body2">{t('terms.whoCanUse1')}</Typography>
+        </div>
+        <div className="btm-50 bullets">
+          <Typography variant="body2">{t('terms.whoCanUse2')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text4')}</Typography>
+          <Typography variant="h4">{t('terms.siteAndAppTitle')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle1')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppText')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text5')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules1')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text6')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text7')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text8')}</Typography>
+          <Typography variant="body2">
+            {t('terms.siteAndAppRules1-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text9')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text10')}</Typography>
-        </div>
-        <div className="btm-20">
-          <Typography variant="body2">{t('terms.text11')}</Typography>
+          <Typography variant="body2">
+            {t('terms.siteAndAppRules1-2')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text12')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules2')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text13')}</Typography>
+          <Typography variant="body2">
+            {t('terms.siteAndAppRules2-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text14')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules3')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text15')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules4')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text16')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules5')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text17')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules6')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle2')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules7')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text18')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules8')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text19')}</Typography>
+          <Typography variant="body2">{t('terms.siteAndAppRules9')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text20')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text21')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text22')}</Typography>
+          <Typography variant="h4">{t('terms.yourAccess')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text23')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text24')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text25')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text26')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text27')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules1')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text28')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text29')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text30')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules2')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text31')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules2-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text32')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules2-2')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text33')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules3')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle3')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules3-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text34')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text35')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text36')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text37')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text38')}</Typography>
-        </div>
-        <div className="btm-20 ml-20">
-          <Typography variant="body2">&bull; {t('terms.text39')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules3-2')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text40')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules4')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle4')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules4-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text41')}</Typography>
+          <Typography variant="body2">{t('terms.yourAccessRules5')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text42')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules5-1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text43')}</Typography>
+          <Typography variant="body2">
+            {t('terms.yourAccessRules5-2')}
+          </Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">{t('terms.yourAccessRules6')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text44')}</Typography>
+          <Typography variant="h4">
+            {t('terms.informationUsageTitle')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle5')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsageText')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text45')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsage1')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text46')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsage2')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text47')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsage3')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text48')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsage4')}
+          </Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">
+            {t('terms.informationUsage5')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text49')}</Typography>
+          <Typography variant="body2">
+            {t('terms.informationUsageFeedback')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle6')}</Typography>
+          <Typography variant="body2">
+            {t('terms.notificationsTitle')}
+          </Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">
+            {t('terms.notificationsText')}
+          </Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text50')}</Typography>
+          <Typography variant="h4">{t('terms.aboutCellPhoneTitle')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle7')}</Typography>
+          <Typography variant="body2">
+            {t('terms.aboutCellPhoneText')}
+          </Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">{t('terms.aboutCellPhone1')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text51')}</Typography>
+          <Typography variant="h4">{t('terms.feesAndDisputes')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="h4">{t('terms.subtitle8')}</Typography>
+          <Typography variant="body2">{t('terms.feesAndDisputes1')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text52')}</Typography>
+          <Typography variant="body2">{t('terms.feesAndDisputes2')}</Typography>
         </div>
         <div className="btm-20">
-          <Typography variant="body2">{t('terms.text53')}</Typography>
+          <Typography variant="body2">{t('terms.feesAndDisputes3')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.feesAndDisputes4')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.feesAndDisputes5')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="h4">{t('terms.updatesToTermsTitle')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">
+            {t('terms.updatesToTermsText')}
+          </Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="h4">{t('terms.notMedicalCareTitle')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">
+            {t('terms.notMedicalCareText')}
+          </Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="h4">{t('terms.otherPoliciesTitle')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">
+            {t('terms.otherPoliciesText')}
+          </Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.otherPolicies1')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.contactTitle')}</Typography>
+        </div>
+        <div className="btm-50">
+          <Typography variant="body2">{t('terms.contactText')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="h4">{t('terms.versionsTitle')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.versionsText')}</Typography>
+        </div>
+        <div className="btm-20">
+          <Typography variant="body2">{t('terms.versionsText1')}</Typography>
         </div>
       </div>
     </ResponsiveStepWrapper>
