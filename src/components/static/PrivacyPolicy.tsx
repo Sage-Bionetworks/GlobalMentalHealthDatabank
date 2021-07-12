19d30ebbf146de2a3e0ed404a253217a39c06e65
William Martinez <william.martinez@genui.com>
2021-07-12 15:36:05 -0600

    updated privacy policy design, adding list numbers and bullets

--- src/components/static/PrivacyPolicy.tsx
+++ src/components/static/PrivacyPolicy.tsx
@@ -32,212 +32,319 @@ export const PrivacyPolicy: FunctionComponent = () => {
               {t('privacyPolicy.privacyLabel.title')}
             </Typography>
           </div>
-
           <div className="btm-20">
-            <Typography variant="h6">
+            <Typography variant="h5">
               {t('privacyPolicy.privacyLabel.collection.title')}
             </Typography>
           </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.collection.question1')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.collection.question2')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.collection.question3')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
+
+          <ol>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.collection.question1')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.collection.question2')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.collection.question3')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
 
         <div className="btm-50">
           <div className="btm-20">
-            <Typography variant="h6">
+            <Typography variant="h5">
               {t('privacyPolicy.privacyLabel.tracking.title')}
             </Typography>
           </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question1')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question2')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question3')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question4')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question5')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesWithConsent')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.tracking.question5')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesWithConsent')}
-            </Typography>
-          </div>
+
+          <ol>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question1')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question2')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question3')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question4')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question5')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.tracking.question6')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
 
         <div className="btm-50">
           <div className="btm-20">
-            <Typography variant="h6">
+            <Typography variant="h5">
               {t('privacyPolicy.privacyLabel.useAndSharing.title')}
             </Typography>
           </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.useAndSharing.question1')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.useAndSharing.question2')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesWithConsent')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.useAndSharing.question3')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.useAndSharing.question4')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.no')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.useAndSharing.question5')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesInTheUS')}
-            </Typography>
-          </div>
+
+          <ol>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.useAndSharing.question1')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.useAndSharing.question2')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yesWithConsent')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.useAndSharing.question3')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.useAndSharing.question4')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.no')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.useAndSharing.question5')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yesInTheUS')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
 
         <div className="btm-50">
           <div className="btm-20">
-            <Typography variant="h6">
+            <Typography variant="h5">
               {t('privacyPolicy.privacyLabel.privacyTools.title')}
             </Typography>
           </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.privacyTools.question1')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.privacyTools.question2')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.privacyTools.question3')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.privacyTools.question4')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
+
+          <ol>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.privacyTools.question1')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.privacyTools.question2')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.privacyTools.question3')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.privacyTools.question4')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
 
         <div className="btm-50">
           <div className="btm-20">
-            <Typography variant="h6">
+            <Typography variant="h5">
               {t('privacyPolicy.privacyLabel.communication.title')}
             </Typography>
           </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.communication.question1')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yes')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.communication.question2')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesIfYouOptIn')}
-            </Typography>
-          </div>
-          <div className="btm-20">
-            <Typography variant="body2" className="italic">
-              {t('privacyPolicy.privacyLabel.communication.question3')}
-            </Typography>
-            <Typography variant="h6">
-              {t('privacyPolicy.privacyLabel.yesIfYouOptIn')}
-            </Typography>
-          </div>
+
+          <ol>
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.communication.question1')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yes')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.communication.question2')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yesIfYouOptIn')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Typography variant="body2">
+                {t('privacyPolicy.privacyLabel.communication.question3')}
+              </Typography>
+              <ul>
+                <li>
+                  <Typography variant="h6">
+                    {t('privacyPolicy.privacyLabel.yesIfYouOptIn')}
+                  </Typography>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
         <a href={PolicyEnglish} target="_blank" rel="noopener noreferrer">
           <div className="btm-20 underlined-link">
